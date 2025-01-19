622cefd89f5ccd0ab2da649b3bd4e706c609892a
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-19 17:03:17 +0100

    create UI

--- lib/presentation/auth/sign_in.dart
+++ lib/presentation/auth/sign_in.dart
@@ -1,11 +1,124 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:football_app/common/widgets/basic_button/basic_button.dart';
 
 class SignInPage extends HookWidget {
   const SignInPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 100, left: 30, right: 30),
+        child: Column(
+          children: [
+            _signInText(),
+            const SizedBox(height: 50),
+            _emailField(),
+            const SizedBox(height: 20),
+            _passwordField(),
+            const SizedBox(height: 50),
+            _signInButton(),
+            const SizedBox(height: 10),
+            _forgotPassword(),
+            const SizedBox(height: 100),
+            _signUp(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _signInText() {
+    return const Text(
+      'SIGN IN',
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    );
+  }
+
+  Widget _emailField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Email',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          decoration: InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _passwordField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Password',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          decoration: InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _signInButton() {
+    return BasicAppButton(
+      onPressed: () {},
+      title: 'Sign In',
+    );
+  }
+
+  Widget _forgotPassword() {
+    return const Text(
+      'Forgot Password?',
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        color: Colors.blue,
+      ),
+    );
+  }
+
+  Widget _signUp() {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Already have an account?',
+          style: TextStyle(
+            color: Colors.black54,
+          ),
+        ),
+        SizedBox(width: 5),
+        Text(
+          'Sign Up',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
   }
 }
