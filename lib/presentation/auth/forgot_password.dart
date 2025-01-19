7127e2524d68093edc3268a910446c1217ca4e98
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-19 22:43:46 +0100

    create UI

---
+++ lib/presentation/auth/forgot_password.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:football_app/common/widgets/basic_appbar/basic_appbar.dart';
+import 'package:football_app/common/widgets/basic_button/basic_button.dart';
+
+class ForgotPasswordPage extends HookWidget {
+  const ForgotPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const BasicAppbar(),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10, left: 30, right: 30),
+        child: Column(
+          children: [
+            _resetPasswordText(),
+            const SizedBox(height: 50),
+            _emailField(),
+            const SizedBox(height: 50),
+            _sendEmailButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _resetPasswordText() {
+    return const Text(
+      'Reset Password',
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    );
+  }
+
+  Widget _emailField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Email',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          decoration: InputDecoration(
+            fillColor: Colors.white,
+            filled: true,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _sendEmailButton() {
+    return BasicAppButton(
+      onPressed: () {},
+      title: 'Send Email',
+    );
+  }
+}
