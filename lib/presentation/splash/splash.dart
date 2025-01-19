d397c1d18a4f907a9bed9255757c5240d20e2c63
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-19 16:19:07 +0100

    added bloc listener

--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:football_app/bloc/splash/splash_cubit.dart';
+import 'package:football_app/bloc/splash/splash_state.dart';
 import 'package:football_app/core/assets/app_images.dart';
+import 'package:football_app/presentation/auth/sign_in.dart';
 
 class SplashPage extends HookWidget {
   const SplashPage({super.key});
@@ -9,11 +13,23 @@ class SplashPage extends HookWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _appLogo(),
-          ],
+        child: BlocListener<SplashCubit, SplashState>(
+          listener: (context, state) {
+            if (state is UnAuthenticated) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const SignInPage(),
+                ),
+              );
+            }
+          },
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _appLogo(),
+            ],
+          ),
         ),
       ),
     );
