ccf6734c5a4217b92d5a9a52bb3fbd253cee666e
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-20 15:44:00 +0100

    added repository

--- lib/domain/repository/auth.dart
+++ lib/domain/repository/auth.dart
@@ -1,6 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:football_app/data/auth/models/user_creation_req.dart';
+import 'package:football_app/data/auth/models/user_signin_req.dart';
 
 abstract class AuthRepository{
 Future<Either> getSignUp(UserCreationReqModels user);
+Future<Either> getSignIn(UserSigninReqModels user);
 }
