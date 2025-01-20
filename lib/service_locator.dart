0703564f83652dce5767fe52e639d41b466e49ed
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-20 14:46:26 +0100

    create service locator and add service, repository and usecase

---
+++ lib/service_locator.dart
@@ -0,0 +1,17 @@
+import 'package:football_app/data/auth/repository/auth.dart';
+import 'package:football_app/data/auth/source/auth_supabase_service.dart';
+import 'package:football_app/domain/repository/auth.dart';
+import 'package:football_app/domain/usecases/sign_up.dart';
+import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initialzeDependencies() async {
+  //services
+  sl.registerSingleton<AuthSupabaseService>(AuthSupabaseServiceImpl(supabaseClient: Supabase.instance.client));
+  //repository
+  sl.registerSingleton<AuthRepository>(AuthRepositoryImpl());
+  //usecases
+  sl.registerSingleton<SignUpUseCase>(SignUpUseCase());
+}
