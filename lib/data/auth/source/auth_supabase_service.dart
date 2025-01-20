4a39cd9709d2d788ecc94bf349e017bf73c855ab
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-20 14:47:50 +0100

    cerate source

---
+++ lib/data/auth/source/auth_supabase_service.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:football_app/data/auth/models/user_creation_req.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract class AuthSupabaseService {
+  Future<Either> getSignUp(UserCreationReqModels user);
+}
+
+class AuthSupabaseServiceImpl extends AuthSupabaseService {
+  final SupabaseClient supabaseClient;
+  AuthSupabaseServiceImpl({required this.supabaseClient});
+
+  @override
+  Future<Either> getSignUp(UserCreationReqModels user) async {
+    try {
+      final returnedData = await supabaseClient.auth.signUp(
+        password: user.password!,
+        email: user.email!,
+        data: {
+          'firstName': user.firstName,
+          'lastName': user.lastName,
+        },
+      );
+      return Right(returnedData.user!.id);
+    } catch (e) {
+      return  Left(e.toString());
+    }
+  }
+}
