e8d27d01bb817d07b872a0b5321334248830e519
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-20 14:47:23 +0100

    create model

---
+++ lib/data/auth/models/user_creation_req.dart
@@ -0,0 +1,13 @@
+class UserCreationReqModels{
+  String ? firstName;
+  String ? lastName;
+  String ? email;
+  String ? password;
+
+  UserCreationReqModels({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.password,
+  });
+}
