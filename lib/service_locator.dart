0cb07a2d741f1a69cebedc23db8051bf1a4e6d05
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-20 17:30:55 +0100

    added usecase

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,6 +1,7 @@
 import 'package:football_app/data/auth/repository/auth.dart';
 import 'package:football_app/data/auth/source/auth_supabase_service.dart';
 import 'package:football_app/domain/repository/auth.dart';
+import 'package:football_app/domain/usecases/reset_password.dart';
 import 'package:football_app/domain/usecases/sign_in.dart';
 import 'package:football_app/domain/usecases/sign_up.dart';
 import 'package:get_it/get_it.dart';
@@ -16,4 +17,5 @@ Future<void> initialzeDependencies() async {
   //usecases
   sl.registerSingleton<SignUpUseCase>(SignUpUseCase());
   sl.registerSingleton<SignInUseCase>(SignInUseCase());
+  sl.registerSingleton<ResetPasswordUseCase>(ResetPasswordUseCase());
 }
