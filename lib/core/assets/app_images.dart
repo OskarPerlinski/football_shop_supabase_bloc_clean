f74ad8a79fe076d22ec9dbab35f58086b8fdf846
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-21 20:41:21 +0100

    added assets

--- lib/core/assets/app_images.dart
+++ lib/core/assets/app_images.dart
@@ -2,4 +2,5 @@ class AppImages {
   static const basePath = 'assets/';
   static const appLogo = '${basePath}rgol.webp';
   static const emailSend = '${basePath}email_send.png';
+  static const rgolBanner = '${basePath}banner.jpg';
 }
