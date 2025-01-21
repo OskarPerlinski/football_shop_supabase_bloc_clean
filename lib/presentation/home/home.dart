b55adba4b7716f3789db8f80f542eb3b9a9e0334
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-21 20:42:09 +0100

    added UI

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:football_app/common/widgets/basic_appbar/basic_appbar.dart';
+import 'package:football_app/core/assets/app_images.dart';
+import 'package:football_app/presentation/home/widget/club_collections.dart';
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: const BasicAppbar(
+        hideBack: true,
+        title: SizedBox(
+          height: 100,
+          child: Image(
+            image: AssetImage(AppImages.appLogo),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 30),
+            _rgolBanner(),
+            const SizedBox(height: 30),
+            const ClubCollections(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _rgolBanner() {
+    return  Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Container(
+          height: 200,
+          decoration: const BoxDecoration(
+            color: Colors.blue,
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(AppImages.rgolBanner),
+            ),
+          ),
+        ),
+    );
   }
 }
