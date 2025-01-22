c42acf23dbc01f6c9f64c0fbe93e27316a7f26f8
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-22 12:43:13 +0100

    create entity

---
+++ lib/domain/brands/entity/brands.dart
@@ -0,0 +1,11 @@
+class BrandsEntity {
+  final String id;
+  final String brand;
+  final String image;
+
+  BrandsEntity({
+    required this.id,
+    required this.brand,
+    required this.image,
+  });
+}
