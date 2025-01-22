edda3f723b1622f17e2a7ae9877278cf5e31da9b
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-22 08:33:34 +0100

    create entity

---
+++ lib/domain/club_collections/entity/club_collections.dart
@@ -0,0 +1,11 @@
+class ClubCollectionsEntity {
+  final String id;
+  final String league;
+  final String image;
+
+  ClubCollectionsEntity({
+    required this.id,
+    required this.league,
+    required this.image,
+  });
+}
