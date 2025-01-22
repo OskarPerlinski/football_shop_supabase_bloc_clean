5a49c49fefae4ca79f2885366a96531ad6df7491
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-22 12:44:28 +0100

    added bloc provider and builder

--- lib/presentation/home/widget/brands.dart
+++ lib/presentation/home/widget/brands.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:football_app/bloc/brands/brands_cubit.dart';
+import 'package:football_app/bloc/brands/brands_state.dart';
+import 'package:football_app/domain/brands/entity/brands.dart';
 
 class Brands extends HookWidget {
   const Brands({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        _brandsText(),
-        const SizedBox(height: 30),
-        _collections(),
-      ],
+    return BlocProvider(
+      create: (context) => BrandsCubit()..displayBrands(),
+      child: BlocBuilder<BrandsCubit, BrandsState>(
+        builder: (context, state) {
+          if (state is BrandsLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is BrandsLoaded) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _brandsText(),
+                const SizedBox(height: 30),
+                _brands(state.brands),
+              ],
+            );
+          }
+          if (state is FailureLoadedBrands) {
+            return const Text(
+              'Failed to load...',
+            );
+          }
+          return Container();
+        },
+      ),
     );
   }
 
@@ -29,7 +53,7 @@ class Brands extends HookWidget {
     );
   }
 
-  Widget _collections() {
+  Widget _brands(List<BrandsEntity> brands) {
     return SizedBox(
       height: 550,
       child: GridView.builder(
@@ -40,7 +64,7 @@ class Brands extends HookWidget {
           mainAxisSpacing: 1,
           childAspectRatio: 0.8,
         ),
-        itemCount: 4,
+        itemCount: brands.length,
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.only(left: 10, right: 10),
@@ -52,15 +76,19 @@ class Brands extends HookWidget {
                   width: double.infinity,
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
-                    color: Colors.blue,
+                    image: DecorationImage(
+                      image: NetworkImage(brands[index].image),
+                    ),
                   ),
                 ),
                 const SizedBox(height: 10),
-                const Text(
-                  'sdsds',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
+                Center(
+                  child: Text(
+                    brands[index].brand,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
                   ),
                 ),
               ],
