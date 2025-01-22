cc160f1f9f15ca079d2c80b4f800212adce54127
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-22 08:32:10 +0100

    added bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:football_app/bloc/club_collections/club_collections_cubit.dart';
 import 'package:football_app/bloc/splash/splash_cubit.dart';
 import 'package:football_app/presentation/splash/splash.dart';
 import 'package:football_app/service_locator.dart';
@@ -26,8 +27,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => SplashCubit()..appStated(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => SplashCubit()..appStated()),
+        BlocProvider(create: (context) => ClubCollectionsCubit()..displayClubCollections()),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
