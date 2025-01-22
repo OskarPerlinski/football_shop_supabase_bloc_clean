7d8d878a466993a73421764220fd65f90c42c404
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-22 12:42:02 +0100

    added services, repo, source

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,5 +1,7 @@
 import 'package:football_app/data/auth/repository/auth.dart';
 import 'package:football_app/data/auth/source/auth_supabase_service.dart';
+import 'package:football_app/data/brands/repository/brands.dart';
+import 'package:football_app/data/brands/source/brands_supabase_source.dart';
 import 'package:football_app/data/club_collections/repository/club_collections.dart';
 import 'package:football_app/data/club_collections/source/club_collections_supabase_source.dart';
 import 'package:football_app/domain/auth/repository/auth.dart';
@@ -7,6 +9,8 @@ import 'package:football_app/domain/auth/usecases/is_logged_in.dart';
 import 'package:football_app/domain/auth/usecases/reset_password.dart';
 import 'package:football_app/domain/auth/usecases/sign_in.dart';
 import 'package:football_app/domain/auth/usecases/sign_up.dart';
+import 'package:football_app/domain/brands/repository/brands.dart';
+import 'package:football_app/domain/brands/usecase/brands.dart';
 import 'package:football_app/domain/club_collections/repository/club_collections.dart';
 import 'package:football_app/domain/club_collections/usecase/club_collections.dart';
 import 'package:get_it/get_it.dart';
@@ -18,13 +22,16 @@ Future<void> initialzeDependencies() async {
   //services
   sl.registerSingleton<AuthSupabaseService>(AuthSupabaseServiceImpl(supabaseClient: Supabase.instance.client));
   sl.registerSingleton<ClubCollectionsSupabaseSource>(ClubCollectionsSupabaseSourceImpl(supabaseClient: Supabase.instance.client));
+  sl.registerSingleton<BrandsSupabaseSource>(BrandsSupabaseSourceImpl(supabaseClient: Supabase.instance.client));
   //repository
   sl.registerSingleton<AuthRepository>(AuthRepositoryImpl());
   sl.registerSingleton<ClubCollectionsRepository>(ClubCollectionsRepositoryImpl());
+  sl.registerSingleton<BrandsReposiotry>(BrandsRepositoryImpl());
   //usecases
   sl.registerSingleton<SignUpUseCase>(SignUpUseCase());
   sl.registerSingleton<SignInUseCase>(SignInUseCase());
   sl.registerSingleton<ResetPasswordUseCase>(ResetPasswordUseCase());
   sl.registerSingleton<IsLoggedInUseCase>(IsLoggedInUseCase());
   sl.registerSingleton<ClubCollectionsUseCase>(ClubCollectionsUseCase());
+  sl.registerSingleton<BrandsUseCase>(BrandsUseCase());
 }
