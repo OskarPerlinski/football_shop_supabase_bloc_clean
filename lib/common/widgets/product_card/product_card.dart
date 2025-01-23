c60342727fb41b2427a8cc3f29d478dba56f6461
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-23 19:37:15 +0100

    added entity

--- lib/common/widgets/product_card/product_card.dart
+++ lib/common/widgets/product_card/product_card.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:football_app/domain/products/entity/products.dart';
 
 class ProductCard extends HookWidget {
-  const ProductCard({super.key});
+  final ProductsEntity productsEntity;
+  const ProductCard({super.key, required this.productsEntity});
 
   @override
   Widget build(BuildContext context) {
@@ -21,33 +23,37 @@ class ProductCard extends HookWidget {
             Expanded(
               flex: 3,
               child: Container(
-                decoration: const BoxDecoration(
+                decoration: BoxDecoration(
                   color: Colors.yellow,
-                  borderRadius: BorderRadius.only(
+                  image: DecorationImage(
+                    image: NetworkImage(productsEntity.images[0]),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: const BorderRadius.only(
                     topLeft: Radius.circular(8),
                     topRight: Radius.circular(8),
                   ),
                 ),
               ),
             ),
-            const Expanded(
+            Expanded(
               flex: 1,
               child: Padding(
-                padding: EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(8.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      'sdsdssd',
-                      style: TextStyle(
+                      productsEntity.name,
+                      style: const TextStyle(
                         fontSize: 14,
                         overflow: TextOverflow.ellipsis,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(height: 5),
+                    const SizedBox(height: 5),
                     Text(
-                      'dsdsdsd',
+                      "${productsEntity.price}\$",
                     ),
                   ],
                 ),
