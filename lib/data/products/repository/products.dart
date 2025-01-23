beb3bdd2530bfda2919020ae10752d463ebce3b7
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-23 19:37:32 +0100

    create repositoryImpl

---
+++ lib/data/products/repository/products.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:football_app/data/products/models/products.dart';
+import 'package:football_app/data/products/source/products_supabase_service.dart';
+import 'package:football_app/domain/products/repository/products.dart';
+import 'package:football_app/service_locator.dart';
+
+class ProductsRepositoryImpl extends ProductsReposiotry {
+  @override
+  Future<Either> getNewProducts() async {
+    var returnedData = await sl<ProductsSupabaseService>().getNewProducts();
+    return returnedData.fold(
+      (error) {
+        return Left(error);
+      },
+      (data) {
+        return Right(List.from(data)
+            .map((e) => ProductsModel.fromMap(e).toEntity())
+            .toList());
+      },
+    );
+  }
+}
