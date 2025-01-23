f69940a350ee3cab0b9a5ffe19d62d318b955c5a
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-23 21:27:10 +0100

    added repositoryImpl

--- lib/data/products/repository/products.dart
+++ lib/data/products/repository/products.dart
@@ -19,4 +19,19 @@ class ProductsRepositoryImpl extends ProductsReposiotry {
       },
     );
   }
+
+  @override
+  Future<Either> getByBrandId(String brand) async {
+    var returnedData = await sl<ProductsSupabaseService>().getByBrandId(brand);
+    return returnedData.fold(
+      (error) {
+        return Left(error);
+      },
+      (data) {
+        return Right(List.from(data)
+            .map((e) => ProductsModel.fromMap(e).toEntity())
+            .toList());
+      },
+    );
+  }
 }
