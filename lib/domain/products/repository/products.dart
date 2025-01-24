da142310b8675396ca6e43d7bc3f50e77d08d94e
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-24 10:12:31 +0100

    added repository

--- lib/domain/products/repository/products.dart
+++ lib/domain/products/repository/products.dart
@@ -3,4 +3,5 @@ import 'package:dartz/dartz.dart';
 abstract class ProductsReposiotry{
   Future<Either> getNewProducts();
   Future<Either> getByBrandId(String brand);
+  Future<Either> getByColletions(String collections);
 }
