0737225944848d678e94d311be2813da440a1be2
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-24 10:14:41 +0100

    added navigator

--- lib/presentation/home/widget/club_collections.dart
+++ lib/presentation/home/widget/club_collections.dart
@@ -4,40 +4,41 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:football_app/bloc/club_collections/club_collections_cubit.dart';
 import 'package:football_app/bloc/club_collections/club_collections_state.dart';
 import 'package:football_app/domain/club_collections/entity/club_collections.dart';
+import 'package:football_app/presentation/get_by_collections_id/get_by_collections.dart';
 
 class ClubCollections extends HookWidget {
   const ClubCollections({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  BlocProvider(
-        create: (context) => ClubCollectionsCubit()..displayClubCollections(),
-        child: BlocBuilder<ClubCollectionsCubit, ClubCollectionsState>(
-          builder: (context, state) {
-            if (state is ClubCollectionsLoading) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if (state is ClubCollectionsLoaded) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _clubCollectionsText(),
-                  const SizedBox(height: 20),
-                  _collections(state.collections),
-                ],
-              );
-            }
-            if (state is FailureLoadClubCollections) {
-              return const Center(
-                child: Text('Failed to load...'),
-              );
-            }
-            return Container();
-          },
-        ),
-      );
+    return BlocProvider(
+      create: (context) => ClubCollectionsCubit()..displayClubCollections(),
+      child: BlocBuilder<ClubCollectionsCubit, ClubCollectionsState>(
+        builder: (context, state) {
+          if (state is ClubCollectionsLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is ClubCollectionsLoaded) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _clubCollectionsText(),
+                const SizedBox(height: 20),
+                _collections(state.collections, context),
+              ],
+            );
+          }
+          if (state is FailureLoadClubCollections) {
+            return const Center(
+              child: Text('Failed to load...'),
+            );
+          }
+          return Container();
+        },
+      ),
+    );
   }
 
   Widget _clubCollectionsText() {
@@ -53,7 +54,8 @@ class ClubCollections extends HookWidget {
     );
   }
 
-  Widget _collections(List<ClubCollectionsEntity> collections) {
+  Widget _collections(
+      List<ClubCollectionsEntity> collections, BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
@@ -61,29 +63,43 @@ class ClubCollections extends HookWidget {
           collections.length,
           (index) => Padding(
             padding: const EdgeInsets.only(right: 10, left: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 300,
-                  width: 250,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage(collections[index].image),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => GetByCollectionsPage(
+                      collectionsEntity: collections[index],
                     ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                Text(
-                  collections[index].league,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
+                );
+              },
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 300,
+                    width: 250,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          collections[index].image,
+                        ),
+                      ),
+                    ),
                   ),
-                )
-              ],
+                  const SizedBox(height: 20),
+                  Text(
+                    collections[index].league,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
