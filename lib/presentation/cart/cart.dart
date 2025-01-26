5fd32501e6a10e9f968fefd0b5b6ab6203f0a5a5
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-26 20:40:31 +0100

    create cartPage

---
+++ lib/presentation/cart/cart.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class CartPage extends HookWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
