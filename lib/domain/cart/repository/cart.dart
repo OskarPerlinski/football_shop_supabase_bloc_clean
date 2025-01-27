2153cc392feafb82f749d7eb017a1522810bd8d0
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-27 19:33:01 +0100

    added repository

--- lib/domain/cart/repository/cart.dart
+++ lib/domain/cart/repository/cart.dart
@@ -4,4 +4,5 @@ import 'package:football_app/data/cart/modelss/cart.dart';
 abstract class CartReposiotry{
   Future<Either> addToCart(CartModel cartModel);
   Future<Either> getCartProducts();
+  Future<Either> getRemoveProducts(String id);
 }
