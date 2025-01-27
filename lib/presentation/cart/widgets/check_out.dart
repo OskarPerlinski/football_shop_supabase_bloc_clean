64b4275cdba5ab25d5f7c1b1e96f3ce1b5ab23ff
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-27 13:05:51 +0100

    create widget

---
+++ lib/presentation/cart/widgets/check_out.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:football_app/common/widgets/basic_button/basic_button.dart';
+import 'package:football_app/common/widgets/product_card/cart_helper.dart';
+import 'package:football_app/domain/cart/entity/product_ordered.dart';
+
+class CheckOut extends HookWidget {
+  final List<ProductOrderedEntity> products;
+  const CheckOut({super.key, required this.products});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+      height: MediaQuery.of(context).size.height / 3.5,
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            const Text(
+              'Subtotal',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 16,
+              ),
+            ),
+            Text(
+              '\$${CartHelper.calculateCartTotalSubtotal(products)}',
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+                color: Colors.black,
+              ),
+            )
+          ]),
+          const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Shiping Cost',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                  ),
+                ),
+                Text(
+                  '\$8',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                )
+              ]),
+           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Total',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                ),
+              ),
+              Text(
+                '\$${CartHelper.calculateCartTotalSubtotal(products) +8}',
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              )
+            ],
+          ),
+          ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 600),
+            child: BasicAppButton(
+              onPressed: () {},
+              title: 'Buy Now',
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
