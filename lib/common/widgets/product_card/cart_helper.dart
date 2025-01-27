66a55fe924fd8d3eb6fb585e2374aaca00ee1d70
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-27 20:24:39 +0100

    little change

--- lib/common/widgets/product_card/cart_helper.dart
+++ lib/common/widgets/product_card/cart_helper.dart
@@ -1,15 +1,13 @@
 import 'package:football_app/domain/cart/entity/product_ordered.dart';
 
 class CartHelper {
-  static double calculateCartTotalSubtotal(
-      List<ProductOrderedEntity> products) {
-    double subtotalPrice = 0.0;
-
+  static double calculateCartTotalSubtotal(List<ProductOrderedEntity> products) {
+    double subtotalPrice = 0;
     for (var item in products) {
-      double itemPrice = double.tryParse(item.price) ?? 0.0;
-      subtotalPrice += itemPrice;
+      subtotalPrice += item.totalPrice.isNotEmpty 
+          ? double.parse(item.totalPrice) 
+          : 0;
     }
-
     return subtotalPrice;
   }
 }
