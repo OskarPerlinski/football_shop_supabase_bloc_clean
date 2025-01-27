833fb8a1fda5a46ecf3a7d8abce8231aad6e032e
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-27 13:01:32 +0100

    create entity

---
+++ lib/domain/cart/entity/product_ordered.dart
@@ -0,0 +1,21 @@
+class ProductOrderedEntity {
+  final String createdAt;
+  final String productId;
+  final String name;
+  final String price;
+  final String images;
+  final String size;
+  final String quantity;
+  final String id;
+
+  ProductOrderedEntity({
+    required this.createdAt,
+    required this.productId,
+    required this.name,
+    required this.price,
+    required this.images,
+    required this.quantity,
+    required this.size,
+    required this.id,
+  });
+}
