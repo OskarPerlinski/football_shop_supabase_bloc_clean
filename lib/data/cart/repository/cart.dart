13e13336359db16cd3f718cd23bd5cc0c46a8713
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-27 13:01:08 +0100

    add repositoryImpl

--- lib/data/cart/repository/cart.dart
+++ lib/data/cart/repository/cart.dart
@@ -1,13 +1,30 @@
 import 'package:dartz/dartz.dart';
 import 'package:football_app/data/cart/modelss/cart.dart';
+import 'package:football_app/data/cart/modelss/product_ordered.dart';
 import 'package:football_app/data/cart/source/cart_supabase_service.dart';
 import 'package:football_app/domain/cart/repository/cart.dart';
 import 'package:football_app/service_locator.dart';
 
-class CartReposiotryImpl extends CartReposiotry{
+class CartReposiotryImpl extends CartReposiotry {
   @override
   Future<Either> addToCart(CartModel cartModel) async {
     return await sl<CartSupabaseService>().addToCart(cartModel);
   }
 
+  @override
+  Future<Either> getCartProducts() async {
+    var returnedData = await sl<CartSupabaseService>().getCartProducts();
+    return returnedData.fold(
+      (error) {
+        return left(error);
+      },
+      (data) {
+        return Right(
+          List.from(data)
+              .map((e) => ProductOrderedModel.fromMap(e).toEntity())
+              .toList(),
+        );
+      },
+    );
+  }
 }
