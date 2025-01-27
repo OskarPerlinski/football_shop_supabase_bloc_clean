ddf9cd909699aee75ad5ab7a999ab745c8a9c56e
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-27 11:50:27 +0100

    create entity

---
+++ lib/domain/cart/entity/cart.dart
@@ -0,0 +1,19 @@
+class CartEntity {
+  final String createdAt;
+  final String productId;
+  final String name;
+  final String price;
+  final String imgaes;
+  final String quantity;
+  final String size;
+
+  CartEntity({
+    required this.createdAt,
+    required this.productId,
+    required this.name,
+    required this.price,
+    required this.imgaes,
+    required this.quantity,
+    required this.size,
+  });
+}
