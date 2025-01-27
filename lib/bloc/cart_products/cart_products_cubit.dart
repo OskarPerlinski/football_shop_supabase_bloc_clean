83b75edb397f1a080a47fa8bd43eeb839b246665
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-27 19:32:12 +0100

    added removed products

--- lib/bloc/cart_products/cart_products_cubit.dart
+++ lib/bloc/cart_products/cart_products_cubit.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:football_app/bloc/cart_products/cart_products_state.dart';
+import 'package:football_app/domain/cart/entity/product_ordered.dart';
 import 'package:football_app/domain/cart/usecases/get_cart_products.dart';
+import 'package:football_app/domain/cart/usecases/get_remove_products.dart';
 import 'package:football_app/service_locator.dart';
 
 class CartProductsCubit extends Cubit<CartProductsState> {
@@ -14,6 +16,19 @@ class CartProductsCubit extends Cubit<CartProductsState> {
       },
       (data) {
         emit(CartProductsLoaded(products: data));
+      },
+    );
+  }
+
+  void removeProducts(ProductOrderedEntity product) async {
+    var returnedData =
+        await sl<GetRemoveProductsUseCase>().call(params: product.id);
+    returnedData.fold(
+      (error) {
+        emit(FailureLoadCartProducts());
+      },
+      (data) {
+        displayProducts();
       },
     );
   }
