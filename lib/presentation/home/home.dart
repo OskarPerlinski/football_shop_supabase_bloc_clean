3101afd0e7f3cdb609dd9e42b8097cf5746b7945
OskarPerliński <oskar.perlinskii@gmail.com>
2025-01-28 16:16:48 +0100

    added iconButton

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:football_app/common/widgets/basic_appbar/basic_appbar.dart';
 import 'package:football_app/core/assets/app_images.dart';
+import 'package:football_app/presentation/cart/cart.dart';
 import 'package:football_app/presentation/home/widget/brands.dart';
 import 'package:football_app/presentation/home/widget/club_collections.dart';
 import 'package:football_app/presentation/home/widget/new_product.dart';
@@ -12,13 +13,34 @@ class HomePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const BasicAppbar(
+      appBar: BasicAppbar(
         hideBack: true,
-        title: SizedBox(
-          height: 100,
-          child: Image(
-            image: AssetImage(AppImages.appLogo),
-          ),
+        title: Stack(
+          children: [
+            const Center(
+              child: SizedBox(
+                height: 100,
+                child: Image(
+                  image: AssetImage(AppImages.appLogo),
+                ),
+              ),
+            ),
+            Positioned(
+              right: 20, 
+              top: 30,
+              child: IconButton(
+                icon: const Icon(Icons.shopping_bag),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const CartPage(),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
       body: SingleChildScrollView(
@@ -39,17 +61,17 @@ class HomePage extends HookWidget {
   }
 
   Widget _rgolBanner() {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.only(right: 20, left: 20),
       child: Container(
-          height: 200,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage(AppImages.rgolBanner),
-            ),
+        height: 200,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(AppImages.rgolBanner),
           ),
         ),
+      ),
     );
   }
 }
